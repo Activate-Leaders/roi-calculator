a0825b9c30675f8e39078e311d0e47e16a42217c
Mike Cheney <mike@activateleaders.co>
2025-03-03 02:22:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const tabs = document.querySelectorAll('.tab');
         const contents = document.querySelectorAll('.tab-content');
 
-        tabs.forEach(tab => tab.classList.remove('active'));
+        tabs.forEach((tab) => tab.classList.remove('active'));
         contents.forEach(content => content.classList.remove('active'));
 
         document.querySelector(`#${tabId}`).classList.add('active');
